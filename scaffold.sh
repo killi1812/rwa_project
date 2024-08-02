c30013fce055c42f1a9773412b03ca4ada07cce4
killi1812 <cvok.fran@gmail.com>
2024-08-02 09:49:28 +0200

    moved to microsoft sql

--- scaffold.sh
+++ scaffold.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export PATH="$PATH:/home/fran/.dotnet/tools"
-dotnet-ef dbcontext scaffold "Name=ConnectionStrings:MySqlDb" MySql.EntityFrameworkCore -o Models --force --project WebApp
+dotnet-ef dbcontext scaffold "Name=ConnectionStrings:MySqlDb" Microsoft.EntityFrameworkCore -o Models --force --project WebApp
 dotnet-ef dbcontext scaffold "Name=ConnectionStrings:MySqlDb" MySql.EntityFrameworkCore -o Models --force --project WebApi
