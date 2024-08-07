732a05c1401223dac1efb52f45434b31f4caf40d
killi1812 <cvok.fran@gmail.com>
2024-08-07 10:26:38 +0200

    Sql bulshit

--- init/init.sql
+++ init/init.sql
@@ -1,45 +1,58 @@
+--TODO mby rename 
 CREATE DATABASE rwa;
 GO
 
 USE rwa;
 GO
 
-CREATE TABLE  users (
-                        id INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
-                        name VARCHAR(255) NOT NULL,
-                        admin bit NOT NULL,
-                        password VARCHAR(255) NOT NULL
+CREATE TABLE users
+(
+    id       INT IDENTITY (1,1) PRIMARY KEY NOT NULL,
+    username     VARCHAR(255)                   NOT NULL,
+    admin    bit                            NOT NULL,
+    password VARCHAR(255)                   NOT NULL
 );
 -- 1-n relationship between users and pictures
-create table pictures (
-                          id int identity(1,1) primary key not null,
-                          name varchar(255) not null,
-                          photographer varchar(255) not null,
-                          userId int not null,
-                          data varbinary(max) not null,
-                          foreign key (userId) references users(id)
+create table pictures
+(
+    id           int identity (1,1) primary key not null,
+    name         varchar(255)                   not null,
+    photographer varchar(255)                   not null,
+    userId       int                            not null,
+    data         varbinary(max)                 not null,
+    foreign key (userId) references users (id)
 );
 
-create table tags (
-                      id int identity(1,1) primary key not null,
-                      name varchar(255) not null
+create table tags
+(
+    id   int identity (1,1) primary key not null,
+    name varchar(255)                   not null
 );
 
 -- m-n relationship between pictures and tags
-create table pictureTags (
-                             pictureId int not null,
-                             tagId int not null,
-                             foreign key (pictureId) references pictures(id),
-                             foreign key (tagId) references tags(id)
+create table pictureTags
+(
+    pictureId int not null,
+    tagId     int not null,
+    foreign key (pictureId) references pictures (id),
+    foreign key (tagId) references tags (id)
 );
 
 -- m-n relationship between users and pictures
-create table downloads (
-                           id int identity(1,1) primary key not null,
-                           date datetime not null,
-                           pictureId int not null,
-                           userId int not null,
-                           foreign key (pictureId) references pictures(id),
-                           foreign key (userId) references users(id)
+create table downloads
+(
+    id        int identity (1,1) primary key not null,
+    date      datetime                       not null,
+    pictureId int                            not null,
+    userId    int                            not null,
+    foreign key (pictureId) references pictures (id),
+    foreign key (userId) references users (id)
 );
 GO
+
+create table logs
+(
+    id      int identity (1,1) primary key not null,
+    date    datetime                       not null,
+    message varchar(255)                   not null,
+);
